7ea162bee1896dcf9370e799d1a2570309052ab0
Caaio-Martins <caio.antnmartins@gmail.com>
2023-12-29 13:43:07 -0300

    Abstração, Encapsulamento, Herança e Polimorfismo

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using ExemploPOO.Models;
+
+
+Aluno a1 = new Aluno();
+a1.Nome = "Caio";
+a1.Idade = 31;
+a1.Email = "caio@teste.com";
+a1.Nota = 10;
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Antonio";
+p1.Idade = 31;
+p1.Salario = 1000;
+p1.Apresentar();
+
+
+
+
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+// c1.ExibirSaldo();
+// c1.Sacar(5000);
+// c1.ExibirSaldo();
+
+
+
+
+//OBJETO PESSOA
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Caio";
+// p1.Idade = 31;
+
+// p1.Apresentar();
