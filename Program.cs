8c691e3d33d64debd64e5fab301be7bfaf4c2630
Caaio-Martins <caio.antnmartins@gmail.com>
2023-12-29 14:06:37 -0300

    Classe Abstrata

--- Program.cs
+++ Program.cs
@@ -1,18 +1,28 @@
 ﻿using ExemploPOO.Models;
 
 
-Aluno a1 = new Aluno();
-a1.Nome = "Caio";
-a1.Idade = 31;
-a1.Email = "caio@teste.com";
-a1.Nota = 10;
-a1.Apresentar();
+Corrente c = new Corrente();
+c.Creditar(500);
+c.ExibirSaldo();
 
-Professor p1 = new Professor();
-p1.Nome = "Antonio";
-p1.Idade = 31;
-p1.Salario = 1000;
-p1.Apresentar();
+
+
+
+
+
+
+// Aluno a1 = new Aluno();
+// a1.Nome = "Caio";
+// a1.Idade = 31;
+// a1.Email = "caio@teste.com";
+// a1.Nota = 10;
+// a1.Apresentar();
+
+// Professor p1 = new Professor();
+// p1.Nome = "Antonio";
+// p1.Idade = 31;
+// p1.Salario = 1000;
+// p1.Apresentar();
 
 
 
